f750c6368a768a9441085b4f4faced2d331f98a4
vini-pontesb <viniciusbragacontatos@gmail.com>
2026-02-23 22:50:12 -0300

    Conditionally rendering lists

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,28 @@
 import './App.css'
 
-//Rendering Lists with props
-function ListItem(props) {
-  return <li>{props.animal}</li>
-}
-
-function List(props) {
+//Conditionally rendering lists
+function Lists(props) {
+  if (!props.animals) {
+    return <div>Loading...</div>;
+  }
+  if (props.animals.length == 0) {
+    return <div>Thera are no animals in the list</div>;
+  }
   return (
     <ul>
       {props.animals.map((animal) => {
-        return <ListItem key={animal} animal={animal} />;
+        return <li key={animal}>{animal}</li>;
       })}
     </ul>
-  );
+  );  
 }
-
 function App() {
-  const animals = ["Lion", "Cow", "Snake", "Lizard"];
-
+  const animals = [];
   return (
     <div>
       <h1>Animals: </h1>
-      <List animals={animals} />
+      <Lists animals={animals}/>
     </div>
   );
 }
-
 export default App
