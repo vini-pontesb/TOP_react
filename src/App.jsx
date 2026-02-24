584fe617c48c331a3228dd9cb9ad4f9b3c14b01b
vini-pontesb <viniciusbragacontatos@gmail.com>
2026-02-23 23:12:07 -0300

    Conditionally rendering lists using && and ternary operator

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,47 @@
 import './App.css'
 
-//Conditionally rendering lists
+//Conditionally rendering lists using && and ternary operator
 function Lists(props) {
-  if (!props.animals) {
-    return <div>Loading...</div>;
-  }
-  if (props.animals.length == 0) {
-    return <div>Thera are no animals in the list</div>;
-  }
   return (
-    <ul>
-      {props.animals.map((animal) => {
-        return <li key={animal}>{animal}</li>;
-      })}
-    </ul>
-  );  
+    <>
+      {!props.animals ? (
+        <div>Loading...</div>
+      ) : props.animals.length > 0 ? (
+        <ul>
+          {props.animals.map((animal) => {
+            return <li key={animal}>{animal}</li>;
+          })}
+        </ul>
+      ) : (<div>There are no animals in the list!</div>
+      )}
+    </>
+  );
 }
+
+//or
+
+function Lists(props) {
+  return (
+    <>
+      {!props.animals && <div>Loading...</div>}
+      {props.animals && props.animals.lenght > 0 && (
+        <ul>
+          {props.animals.map((animal) => {
+            return <li key={animal}>{animal}</li>
+          })}
+        </ul>
+      )}
+      {props.animals && props.animals.length === 0 && <div>There are no animals in the list!</div>}
+    </>
+  )
+}
+
 function App() {
   const animals = [];
   return (
     <div>
       <h1>Animals: </h1>
-      <Lists animals={animals}/>
+      <Lists animals={animals} />
     </div>
   );
 }
