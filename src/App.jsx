98a66948e661e671ea12bd0586421d3c8294c25d
vini-pontesb <viniciusbragacontatos@gmail.com>
2026-02-27 10:17:06 -0300

    Using map and keys

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,19 @@
 import './App.css'
 
-//Conditionally rendering lists using && and ternary operator
-function Lists(props) {
-  return (
-    <>
-      {!props.animals ? (
-        <div>Loading...</div>
-      ) : props.animals.length > 0 ? (
-        <ul>
-          {props.animals.map((animal) => {
-            return <li key={animal}>{animal}</li>;
-          })}
-        </ul>
-      ) : (<div>There are no animals in the list!</div>
-      )}
-    </>
-  );
-}
+const todos = [
+  {task: "mow the yard", id: crypto.randomUUID()},
+  {task: "Work on Odin Projects", id: crypto.randomUUID()},
+  { task: "Feed the cat", id: crypto.randomUUID() }
+];
 
-//or
-
-function Lists(props) {
+function ToDoList() {
   return (
-    <>
-      {!props.animals && <div>Loading...</div>}
-      {props.animals && props.animals.lenght > 0 && (
-        <ul>
-          {props.animals.map((animal) => {
-            return <li key={animal}>{animal}</li>
-          })}
-        </ul>
-      )}
-      {props.animals && props.animals.length === 0 && <div>There are no animals in the list!</div>}
-    </>
+    <ul>
+      {todos.map((todo) => {
+        <li key={todo.id}>{todo.task}</li>
+      })}
+    </ul>
   )
 }
 
-function App() {
-  const animals = [];
-  return (
-    <div>
-      <h1>Animals: </h1>
-      <Lists animals={animals} />
-    </div>
-  );
-}
 export default App
