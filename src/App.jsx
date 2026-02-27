26311d4532e1b72b13fd3ce73522e43f80f29cbb
vini-pontesb <viniciusbragacontatos@gmail.com>
2026-02-27 11:11:44 -0300

    Passing functions as props

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,32 @@
 import './App.css'
 
-// Using props to pass data to the Button component
+// Passing functions as props
 function Button(props) {
   const buttonStyle ={
     color: props.color,
     fontSize: props.fontSize + "px"
   };
   return (
-    <button style={buttonStyle}>{props.text}</button>
+    <button style={buttonStyle} onClick={props.handleClick}>
+      {props.text}
+    </button>
   );
 }
 
 export default function App() {
+
+  const handleButtonClick = (url) => {
+    window.location.href = url;
+  };
+
   return (
+    <>
+    <h1>Buttons</h1>
     <div>
-      <Button text="Click me!" color="blue" fontSize={16} />
-      <Button text="Don't click me!" color="red" fontSize={24} />
-      <Button text="Click me!" color="blue" fontSize={20} />
+      <Button text="Go to GitHub" color="gray" fontSize={16} handleClick={() => handleButtonClick("https://github.com")} />
+      <Button text="Go to Google" color="red" fontSize={16} handleClick={() => handleButtonClick("https://google.com")} />
+      <Button text="Go to LinkedIn" color="blue" fontSize={16} handleClick={() => handleButtonClick("https://linkedin.com")} />
     </div>
-  )
+    </>
+  );
 }
