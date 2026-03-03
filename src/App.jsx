e838c426e6dc64c3fae00f3f6beb368a5d6ba91a
vini-pontesb <viniciusbragacontatos@gmail.com>
2026-03-03 10:36:33 -0300

    Using components in React

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,14 @@
-import React, { useState } from "react";
 import "./App.css";
-
-const COLORS = ["pink", "green", "blue", "yellow", "purple"];
+import Button from "./components/Button";
+import Header from "./components/Header";
 
 export default function App() {
-  const [backgroundColor, setBackgroundColor] = useState(COLORS[0]);
-
-  const onButtonClick = (color) => () => {
-    setBackgroundColor(color);
-  };
 
   return (
-    <div
-      className="App"
-      style={{
-        backgroundColor
-      }}
-    >
-      {COLORS.map((color) => (
-        <button
-          type="button"
-          key={color}
-          onClick={onButtonClick(color)}
-          className={backgroundColor === color ? "selected" : ""}
-        >
-          {color}
-        </button>
-      ))}
-    </div>
+    <>
+      <Header />  
+      <Button />
+    </>
   );
+    
 } 
---
+++ src/components/Button.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+const COLORS = ["pink", "green", "blue", "yellow", "purple"];
+
+export default function Button() {
+    const [backgroundColor, setBackgroundColor] = useState(COLORS[0]);
+
+    const onButtonClick = (color) => () => {
+        setBackgroundColor(color);
+    };
+    return (
+        <div
+            className="App"
+            style={{
+                backgroundColor
+            }}
+        >
+            {COLORS.map((color) => (
+                <button
+                    type="button"
+                    key={color}
+                    onClick={onButtonClick(color)}
+                    className={backgroundColor === color ? "selected" : ""}
+                >
+                    {color}
+                </button>
+            ))}
+        </div>
+    )
+}
